e3da519ff2762c988593c6b71eae37e140d83632
Eugen Bobrowski <ecologistkai@gmail.com>
2016-06-13 22:26:07 +0300

    Initial

---
+++ fields-for-all.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package Fields_For_All
+ * @version 1.6
+ */
+/*
+Plugin Name: Fields For All
+Plugin URI: http://wordpress.org/plugins/fields-for-all/
+Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
+Author: Eugen Bobrowski
+Version: 1.0
+Author URI: http://atf.li
+*/
